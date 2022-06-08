75a70388280d16aa040e752ac5b40b893b28d3c1
sriganesh.d <sriganesh13091999@gmail.com>
2022-06-08 19:19:56 +0530

    Qmerty classes are added

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -5,10 +5,15 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features =  "src/test/resources/Login/feature",
-        glue = {"Definitions"}
+        features =  "src/test/resources/Login.feature",
+        glue = {"Definitions"},
+        plugin = {
+                "pretty",
+                "json:target/cucumber-json-report.json","html:target/cucumber-ui-reports.html",
+                "rerun:target/failedrerun.txt"
+        }
 )
 
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests{
 
 }
